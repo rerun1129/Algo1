816101cd3736f0844fd3230bd0f4183034681f0c
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-07-15 16:49:07 +0900

    1

--- src/Practice/Code01.java
+++ src/Practice/Code01.java
@@ -1,17 +1,22 @@
 package Practice;
 
+
 public class Code01 {
     public static void main(String[] args) {
 
-        int result = func(4);
-        System.out.println(result);
+        int [] data1 = new int[3];
+        data1[0] = 5;
+        data1[1] = 4;
+        data1[2] = 3;
+        System.out.println(func(3,data1));
     }
 
-    public static int func(int n) {
-        if (n == 0)
+    public static int func(int n, int [] data) {
+        if(n == 0)
             return 0;
         else {
-           return n + func(n - 1);
+
+            return func(n-1,data) + data[n-1];
         }
     }
 }
