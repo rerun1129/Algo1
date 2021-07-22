8b781e4f791216848b8193833f35cc5fa4619dc4
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-07-22 21:15:51 +0900

    빠블 소트

---
+++ src/정렬/BubbleSort.java
@@ -0,0 +1,31 @@
+package 정렬;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {29, 10, 14, 37, 13};
+
+        BS(arr, 5);
+
+        for (int j : arr) System.out.print(j + " ");
+
+    }
+
+
+    public static void BS(int[] arr, int n) {
+        int tmp = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                tmp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = tmp;
+            }
+            if (n == 2) {
+                break;
+            }
+            BS(arr, n - 1);
+        }
+    }
+}
